74329ecf17af4343e28d44c215c88aa5f42d0b6f
darksideyt762 <darksidegaming762@gmail.com>
2024-12-19 19:16:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,8 @@ pkg upgrade -y
 pkg install python -y
 
 # Move the Python script to the Termux bin folder for easy access
-mv ~/storage/downloads/ds_paktool.py $PREFIX/bin/ds_paktool
-chmod +x $PREFIX/bin/ds_paktool
+mv ~/storage/downloads/ds_paktool.py $PREFIX/bin/ds_paktool.py
+chmod +x $PREFIX/bin/ds_paktool.py
 
-# Run the Python script to set up the directories
-ds_paktool
-
-echo "Installation complete! Type 'ds_paktool' to unpack .pak files."
+# Make sure the Python script is executable with the python command
+echo "Installation complete! Type 'python ds_paktool.py' to unpack .pak files."
